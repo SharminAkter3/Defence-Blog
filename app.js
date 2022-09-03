778df46be1680f663500b9b49f0e1056e94e7bf9
SharminAkter3 <mssharmin4145@gmail.com>
2022-09-02 18:31:50 -0700

    added-header

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('connect');
+const loadNavbar = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/news/categories');
+    const data = await res.json();
+    displayNav(data.data.news_category);
+}
+const displayNav = navber => {
+    const navContainer = document.getElementById("nav-container");
+
+    navber.forEach(nav => {
+        const { category_name } = nav;
+        const li = document.createElement('li');
+        li.classList.add('nav-item')
+        li.innerHTML = `
+            <a class="nav-link fs-3" href="#">${category_name}</a>
+            `;
+
+        navContainer.appendChild(li);
+    })
+};
+
+loadNavbar();
+
+
+
