7b18bc1e1e79535fe52a4cea1a7de2849c34d75a
SharminAkter3 <mssharmin4145@gmail.com>
2022-09-03 03:02:01 -0700

    added-news-portal-section

--- app.js
+++ app.js
@@ -1,73 +1,117 @@
-// const loadNavbar = async () => {
-//     const res = await fetch('https://openapi.programming-hero.com/api/news/categories');
+const loadNav = async () => {
+    const response = await fetch("https://openapi.programming-hero.com/api/news/categories");
+    const data = await response.json();
+    // return data;
+    displayNav(data.data.news_category);
+}
+const displayNav = navbar => {
+    // console.log(navbar);
+    const navContainer = document.getElementById("nav-bar")
+    navbar.forEach(nav => {
+        const { category_name } = nav;
+        const li = document.createElement('li');
+        li.classList.add('nav-item')
+        li.innerHTML = `<button onclick="loadButton()" class=" fs-4 mb-4 mt-4 ms-4" >${category_name}</button>
+        `;
+        navContainer.appendChild(li);
+    })
+}
+loadNav();
+
+const loadAllNews = async () => {
+    const res = await fetch("https://openapi.programming-hero.com/api/news/category/08");
+    const data = await res.json();
+    // return data;
+    displayCardNews(data.data);
+}
+
+
+// spinner 
+// const spinner = document.getElementById("spinner");
+// spinner.classList.remove("d-none");
+
+const displayCardNews = mynews => {
+
+    const spinner = document.getElementById("spinner");
+    spinner.classList.add("d-none");
+
+    // console.log(mynews);
+
+    const cardContainer = document.getElementById('card-container');
+    cardContainer.textContent = ' ';
+    mynews.forEach(news => {
+        const { title, details, author, thumbnail_url, total_view } = news;
+        const { img, name } = author;
+        const card = document.createElement('div');
+        card.classList.add('row')
+        card.classList.add('border')
+        card.classList.add('mb-4')
+        card.innerHTML = `
+    <div class="col-md-4 w-full">
+            <img src="${thumbnail_url}" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+        <div class="card-body">
+        <h5 id='cardFirstTitle' class="fs-2 ">${title}</h5>
+        <p class="card-text fs-5">${details.length > 200 ? details.slice(0, 200) + '...' : details
+            }</p >
+        </div >
+        <div id="author-container" class="d-flex mt-4">
+        <div id='author-img' class="col d-flex" >
+        
+        <img src="${img}"class="img-fluid" style="height: 50px; alt="...">
+        <div>
+         <h6 class="card-title mx-2">${name ? name : 'No data Avilable'} </h6> </br>
+        </div >
+       </div >
+       <div class="col"><i class="far fa-eye"></i>
+        ${total_view ? total_view : 0}
+  </div> 
+  <div class='me-4' id=''>
+  <i class="fas fa-star"></i>
+  <i class="fas fa-star"></i>
+  <i class="fas fa-star"></i>
+  <i class="fas fa-star"></i>
+  <i class="fas fa-star"></i>
+  </div>
+ 
+  <div class="col">
+  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Details
+</button>
+  </div>
+        </div >
+    </div >
+    `;
+        cardContainer.appendChild(card);
+    })
+    // category count 
+    document.getElementById("category-count").innerText = mynews.length;
+}
+
+// const loadModal = async () => {
+//     const res = await fetch("https://openapi.programming-hero.com/api/news/0282e0e58a5c404fbd15261f11c2ab6a");
 //     const data = await res.json();
-//     displayNav(data.data.news_category);
+//     // return data;
+//     showModal(data.data[0]);
 // }
-// const displayNav = navber => {
-//     const navContainer = document.getElementById("nav-container");
+// const showModal = modal => {
+//     // console.log(modal);
+//     const { thumbnail_url, details, title } = modal
 
-//     navber.forEach(nav => {
-//         const { category_id, category_name } = nav;
-//         const li = document.createElement('li');
-//         li.classList.add('nav-item')
-//         li.innerHTML = `
-//             <a onclick = loadAllNews('${category_id}')
-//              class="nav-link fs-3" href="#">${category_name}</a>
-//             `;
-//         navContainer.appendChild(li);
-//     })
-// };
-// //onclick = loadAllNews('${category_id}')
-// // loadNavbar();
+//     const modalBody = document.getElementById("modal-body");
+//     modalBody.textContent = "";
+//     modalBody.innerHTML = `
+//     <img src="${thumbnail_url}"/>
+//     <p class="py-4">${title}</p>
 
-
-// const loadAllNews = async () => {
-//     const res = await fetch(`https://openapi.programming-hero.com/api/news/categories`);
-//     const data = await res.json();
-//     displayCardNews(data.data.category_id);
+//     <p class="py-4">${details.length > 500 ? details.slice(0, 500) + '...' : details
+//         } </p>
+//     `;
 // }
+loadModal();
+loadAllNews();
 
-// const displayCardNews = myNews => {
-//     console.log(myNews);
-//     const cardcontainer = document.getElementById('card-container');
-//     myNews.forEach(news => {
-//         const { title, details, author, thumbnail_url } = news;
-//         const { img, name, published_date, total_view } = author;
-//         const card = document.createElement('div');
-//         card.classList.add('row')
-//         card.innerHTML = `
-//         < div class= "col-md-4" >
-//            <img src="${thumbnail_url}" class="img-fluid rounded-start" alt="...">
-//         </div>
-//         <div class="col-md-8">=
-//           <div class="card-body">
-//               <h5 class="card-title fs-2">${title}</h5>
-//               <p class="card-text fs-4">${details.length > 100 ? details.slice(0, 100) + '...' : details}</p>
-//           </div>
-//           <div id="author-container" class= "d-flex">
-//           <div id= "author-img" class ="d-flex">
-//             <img src = "${img}" class= "img-fluid" style ="height: 50px; alt= "...">
-//               <div>
-//                  <h6 class = "card-title mx-2"> ${name}</h6> <br>
-//                  <h6 class = "mx-2">${published_date}</h6>
-//               </div>
-//           </div>
-//           <div class = "col">${total_view ? total_view : 0}</div>
+// loadButton = () => {
 
-//             <div class = "col">
-//                <button class = "btn btn-primary" type = "submit"> Details </button>
-//             </div>
-//           </div>
-//         </div>
-//         `;
-//         cardcontainer.appendChild(card);
-//     })
 // }
-// loadNavbar();
-
-// // const displayCardNews = myNews => {
-// //     const url = `https://openapi.programming-hero.com/api/news/category/'${myNews}'`;
-// //     console.log(url);
-// // }
-// // loadNavbar();
-
